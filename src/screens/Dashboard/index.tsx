5936fe0ab1911eb94a76680baf914705892fb7cf
Everton Oliveira <tonn.oliveira2011@gmail.com>
2021-08-10 14:58:16 -0300

    Configurado styled-components

---
+++ src/screens/Dashboard/index.tsx
@@ -0,0 +1,13 @@
+
+import React from 'react';
+
+import { Text, View, StyleSheet } from 'react-native';
+import { Container } from './styles';
+
+export function Dashboard(){
+    return(
+        <Container>
+            <Text>Dashboard</Text>
+        </Container>
+    )
+}
